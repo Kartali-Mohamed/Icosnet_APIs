ce6162de43b2c8596ebe26c180b8cce3ce1c0358
kartali mohamed <mohameekartali@gmail.com>
2023-11-15 22:29:58 +0100

    modifie custom error views

--- main_project/utils/error_view.py
+++ main_project/utils/error_view.py
@@ -2,12 +2,12 @@ from django.http import JsonResponse
 
 def handler404(request,exception):
     message = ('Path not found')
-    response = JsonResponse(data={'message':message})
+    response = JsonResponse(data={'detail':message})
     response.status_code = 404
     return response
 
 def handler500(request):
     message = ('Internal server error.')
-    response = JsonResponse(data={'message':message})
+    response = JsonResponse(data={'detail':message})
     response.status_code = 500
     return response
